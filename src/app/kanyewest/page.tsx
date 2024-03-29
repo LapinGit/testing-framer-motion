65eea40a0c97e1137b9671af2f71a13b1d86d84f
LapinGit <lapingithub@gmail.com>
2024-03-29 18:25:02 +0800

    Added new details on kanye webpage

--- src/app/kanyewest/page.tsx
+++ src/app/kanyewest/page.tsx
@@ -20,18 +20,35 @@ const page = () => {
   });
 
   return (
-    <div className="bg-slate-400	">
-      {KanyeData.map((kane, i) => (
-        <motion.div
-          initial={"initial"}
-          animate={"animate"}
-          variants={variants()}
-          key={i}
-          className="flex items-center justify-center "
-        >
-          <motion.img src={kane.src} />
-        </motion.div>
-      ))}
+    <div>
+      <div className="bg-slate-400	">
+        {KanyeData.map((kane, i) => (
+          <motion.div
+            initial={"initial"}
+            animate={"animate"}
+            variants={variants()}
+            key={i}
+            className="flex items-center justify-center "
+          >
+            <motion.img src={kane.src} />
+          </motion.div>
+        ))}
+      </div>
+      <div className=" h-screen flex flex-col lg:flex-row relative pt-5 bg-black">
+        <div className="section-cards__item headline__word  h-1/2 lg:h-full w-full lg:w-1/2 flex   t-h-3 ">
+          <div className="pt-12 ">
+            <div>GRAMMY</div>
+            <div>award-winning</div>
+            <div> American rapper</div>
+            <div>producer, singer</div>
+            <div> author, and </div>
+            <div>fashion designer</div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 h-1/2 lg:h-full flex justify-center items-center no-scrollbar bg-black">
+          <img src="/graduation.jpeg" width="660" height="2400" alt="Graduation album" className="section-cards__item_grad"/>
+        </div>
+      </div>
     </div>
   );
 };
