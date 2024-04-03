87a933f6a0e99a076fb208aa73befaa1057cd1eb
LapinGit <lapingithub@gmail.com>
2024-04-03 14:48:19 +0800

    added global css config/ added details leejinah page

--- src/app/leejinah/page.tsx
+++ src/app/leejinah/page.tsx
@@ -1,21 +1,54 @@
-"use client"
-import React from 'react'
-import {motion} from 'framer-motion'
-import { containerVariants } from '@/utils/animation'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { containerVariants } from "@/utils/animation";
 
 const Leejinah = () => {
   return (
-    <div className="flex items-center justify-center self-center bg-violet-500	">
-    <motion.img
-      variants={containerVariants(0.5)}
-      initial="offscreen"
-      whileInView={"onscreen"}
-      src="Leejinah.png"
-      alt="Leejinah"
-      className='w-1/4 mt-5 rounded-full '
-    />
-  </div>
-  )
-}
+    <div>
+      <div className="flex items-center justify-center self-center bg-violet-500">
+        <motion.img
+          variants={containerVariants(0.5)}
+          initial="offscreen"
+          whileInView={"onscreen"}
+          src="Leejinah.png"
+          alt="Leejinah"
+          className="w-1/4 mt-5 rounded-full"
+        />
+      </div>
+      <div className=" flex">
+        <div className="embla-viewport js-embla-viewport is-draggable embla--is-ready bg-indigo-400 	 ">
+          <div className="embla-container ">
+            <div className="embla-slide section-press__slide is-selected">
+              <p className="section-press__slide__excerpt">
+              PANEL TEST
 
-export default Leejinah
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="embla-viewport js-embla-viewport is-draggable embla--is-ready bg-yellow-200 	 ">
+          <div className="embla-container  ">
+            <div className="embla-slide section-press__slide is-selected">
+              <p className="section-press__slide__excerpt">
+           PANEL TEST
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="embla-viewport js-embla-viewport is-draggable embla--is-ready bg-lime-300		 ">
+          <div className="embla-container  ">
+            <div className="embla-slide section-press__slide is-selected">
+              <p className="section-press__slide__excerpt">
+              PANEL TEST
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Leejinah;
